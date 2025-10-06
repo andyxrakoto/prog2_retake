65a0a027e629aabd3e8eec35eec7f9fed3fdf292
andyxrakoto <hei.andinirina@gmail.com>
2025-10-06 11:55:49 +0300

    feat: add class post

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,3 +1,24 @@
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Post {
+    private String id;
+    private User Owner;
+    private List<Message> Messages;
+
+    public Post(String id, User owner, List<Message> messages) {
+        super()
+        this.id = id;
+        this.Owner = owner;
+        this.Messages = new ArrayList<>();
+    }
+
+    public void addMessage(Message message){
+        this.Messages.add(message);
+    }
+
+
 
 }
